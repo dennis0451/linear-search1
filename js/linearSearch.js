61f3932dfbb31727fda39b8cbe21fc797f8b7779
Dennis Corral <dennis.corral@outlook.com>
2022-05-24 17:25:30 -0400

    completed algo

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,35 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+    x = valueToFind
+    y = arrayToSearchThrough
+    z = []
+    for(let i = 0; i < y.length; i++) {
+        if(x === y[i]){
+            z.push(i);
+        }
+    }
+    if(z.length === 1){
+        return z[0]
+    }else if(z.length === 0){
+        return undefined
+    }else{
+    return z
+    }
 };
 
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+    x = valueToFind
+    y = arrayToSearchThrough
+    z = []
+    for(let i = 0; i < y.length; i++) {
+        if(x === y[i]){
+            z.push(i);
+        }
+    }
+    if(z.length === 0){
+        return undefined
+    }else{
+    return z
+    }
+};
