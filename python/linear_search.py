a218a1150fa25bf696dd6e63cea31b094ba3f759
Dennis Corral <dennis.corral@outlook.com>
2022-05-25 18:03:31 -0400

    first solution

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,21 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    answer_list = []
+    # for i in array_to_search_through:
+    #     if value_to_find == i :
+    #         return array_to_search_through.index(i)
+    for i in range(0,len(array_to_search_through)):
+        if value_to_find == array_to_search_through[i]:
+            return i              
+
+    
+
+def linear_search_global(value_to_find, array_to_search_through):
+    answer_list = []
+    for i in range(0,len(array_to_search_through)):
+        if value_to_find == array_to_search_through[i]:
+            answer_list.append(i)
+    return answer_list 
+
+print(linear_search(3, [1,3,2,3]))
